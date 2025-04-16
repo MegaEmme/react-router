26e7a62b143a060730b7a1d8425482628c100497
MegaEmme <mikmusco@gmail.com>
2025-04-16 14:20:10 +0200

    feat: bonus 3

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,15 +1,40 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Posts = () => {
-    useEffect(() => {
-        console.log('mount component Posts')
-        return () => {
-            console.log('unmount component Posts')
-        }
-    }, [])
+    // useEffect(() => {
+    //     console.log('mount component Posts')
+    //     return () => {
+    //         console.log('unmount component Posts')
+    //     }
+    // }, [])
+
+    const [postsData, setPostsData] = useState([])
+
+    const endpoint = "https://jsonplaceholder.typicode.com/posts";
+
+    function getPosts() {
+        axios.get(endpoint)
+            .then(res => {
+                setPostsData(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+                alert('errore')
+            })
+    }
+    useEffect(getPosts, []);
+    console.log(postsData);
 
     return <div>
-        <h1>Posts</h1>
+        <h1>Lista Posts</h1>
+        {postsData.map((element) => (
+            <div key={element.id}>
+                <h2>{element.title}</h2>
+                <p>{element.body}</p>
+                <h3>userId:{element.userId}</h3>
+            </div>
+        ))}
     </div>
 }
 
